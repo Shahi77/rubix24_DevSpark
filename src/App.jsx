cc62b4e3296901ba4072738dbe9bca8a0e48bf4f
Shahi77 <shahiinfinity02@gmail.com>
2024-01-18 23:55:11 +0530

    empty files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import MainPage from "./Pages/MainPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;

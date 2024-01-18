fd2d8746ca41723d64526ad56629c20cb2e70fc8
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2024-01-18 19:22:37 +0530

    add login button functionality for redirection

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,34 @@
 import { Link } from "react-router-dom";
 import { gradientBackgroundStyle } from "../utils/constants";
+import { useState } from "react";
 
 const Header = () => {
+  const [isLogin, setIsLogin] = useState(false);
+
+  const handleLoginClick = () => {
+    setIsLogin(!isLogin);
+  };
+
   return (
     <nav
       className="flex justify-between p-3 shadow-md"
       style={gradientBackgroundStyle}
     >
-      <img src="./logo.png" alt="logo" className="w-11" />
-      <h1 className="m-auto font-bold text-lg">OTT Dashboard</h1>
+      <div>
+        <img src="./logo.png" alt="logo" className="w-11" />
+      </div>
+      <div className="m-auto">
+        <h1 className="font-bold text-lg w-36 text-center">OTT Dashboard</h1>
+      </div>
       <div>
         <ul>
-          <Link to={"/dashboard"}>
-            <li className="font-semibold border-0 shadow-xl rounded-md px-2 py-1 text-black bg-white m-auto cursor-pointer hover:bg-[#0066ff] hover:text-white hover:shadow-2xl transition-all duration-300 ease-in-out">
-              Get Started
-            </li>
+          <Link to={isLogin === false ? "/dashboard" : "/"}>
+            <button
+              className="w-20 mt-1 text-center font-semibold border-0 shadow-xl rounded-md px-2 py-1 text-black bg-white hover:bg-[#0066ff] hover:text-white hover:shadow-2xl transition-all duration-300 ease-in-out"
+              onClick={handleLoginClick}
+            >
+              {isLogin === false ? "Login" : "Logout"}
+            </button>
           </Link>
         </ul>
       </div>

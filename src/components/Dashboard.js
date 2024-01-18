8ff58733260a348d553b311a1bdd1b923b9d4bb2
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2024-01-18 23:55:36 +0530

    feat: add ott platform cards in dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,33 @@
+import { waveBackgroundStyle } from "../utils/constants";
 import Header from "./Header";
+import OttCard from "./OttCard";
 
 const Dashboard = () => {
   return (
     <>
       <Header />
-      <div className="min-h-screen">Dashboard</div>
+      <div
+        className="min-h-screen flex flex-wrap px-48 items-center"
+        style={waveBackgroundStyle}
+      >
+        <ul className="flex list-none space-x-8">
+          <li>
+            <OttCard title={"Amazon Prime"} />
+          </li>
+          <li>
+            <OttCard title={"Netflix"} />
+          </li>
+          <li>
+            <OttCard title={"Sony Liv"} />
+          </li>
+          <li>
+            <OttCard title={"Disney+ Hotstar"} />
+          </li>
+          <li>
+            <OttCard title={"Zee5"} />
+          </li>
+        </ul>
+      </div>
     </>
   );
 };
---
+++ src/components/OttCard.js
@@ -0,0 +1,14 @@
+const OttCard = ({ title }) => {
+  return (
+    <div className="m-4 w-[250px] h-[300px] overflow-hidden rounded-lg transform transition-transform scale-100 hover:scale-95">
+      <img
+        src={`./images/${title}.jpg`}
+        alt="{title}"
+        className="rounded-xl w-full h-48"
+      />
+      <h3 className="px-2 py-1 text-lg font-semibold">{title}</h3>
+    </div>
+  );
+};
+
+export default OttCard;
